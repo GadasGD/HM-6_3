5504a7ba8f55a64ed12ab91cc419d99b4618e5a9
gadas <gadas4415@gmail.com>
2023-10-28 14:54:41 +0700

    final

--- main.c
+++ main.c
@@ -1,21 +1,23 @@
+//The program solving the rebus by bruteforce
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-    double L = 1, O = 1, G = 1, I = 1, C = 1;
-    for (;pow(L + O + G + I + C, 3)  == L * 10000 + O * 1000 + G * 100 + I * 10 + C; L++)
-    {
-        for (;pow(L + O + G + I + C, 3)  == L * 10000 + O * 1000 + G * 100 + I * 10 + C; O++){
-            for (;pow(L + O + G + I + C, 3)  == L * 10000 + O * 1000 + G * 100 + I * 10 + C; G++){
-                for (;pow(L + O + G + I + C, 3)  == L * 10000 + O * 1000 + G * 100 + I * 10 + C; I++){
-                    for (;pow(L + O + G + I + C, 3)  == L * 10000 + O * 1000 + G * 100 + I * 10 + C; C++){
-                        printf("%f%f%f%f%f", L, O, G, I, C);
+    int L = 1, O = 1, G = 1, I = 1, C = 1;
+    for (L = 0; L < 10; L++) {
+        for (O = 0; O < 10; O++) {
+            for (G = 0; G < 10; G++) {
+                for (I = 0; I < 10; I++) {
+                    for (C = 0; C < 10; C++) {{
+                        if(pow(L + O + G + I + C, 3) == L * 10000 + O * 1000 + G * 100 + I * 10 + C){
+                            printf("%d %d %d %d %d\n", L, O, G, I, C);
+                        }
+                        }
                     }
                 }
             }
         }
     }
-    
     return 0;
 }
